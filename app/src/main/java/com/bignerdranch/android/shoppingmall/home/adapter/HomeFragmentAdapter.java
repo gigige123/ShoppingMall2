df98816105d4f45507c992839ae3dd56eee245d7
gigige123 <junjun_ch@163.com>
2017-01-10 09:04:15 +0800

    just for test

--- app/src/main/java/com/bignerdranch/android/shoppingmall/home/adapter/HomeFragmentAdapter.java
+++ app/src/main/java/com/bignerdranch/android/shoppingmall/home/adapter/HomeFragmentAdapter.java
@@ -1,12 +1,20 @@
 package com.bignerdranch.android.shoppingmall.home.adapter;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bignerdranch.android.shoppingmall.R;
@@ -18,8 +26,13 @@ import com.youth.banner.BannerConfig;
 import com.youth.banner.Transformer;
 import com.youth.banner.listener.OnBannerClickListener;
 import com.youth.banner.listener.OnLoadImageListener;
+import com.zhy.magicviewpager.transformer.AlphaPageTransformer;
+import com.zhy.magicviewpager.transformer.RotateDownPageTransformer;
+import com.zhy.magicviewpager.transformer.ScaleInTransformer;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -62,10 +75,166 @@ public class HomeFragmentAdapter extends RecyclerView.Adapter {
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
        if(viewType == BANNER){
             return new BannerViewHolder(mContext,mLayoutInflater.inflate(R.layout.banner_viewpager,null));
+       }else if(viewType == CHANNEL){
+            return new ChannelViewHolder(mContext,mLayoutInflater.inflate(R.layout.channel_item,null));
+       }else if(viewType == ACTION){
+           return new ActionViewHolder(mContext,mLayoutInflater.inflate(R.layout.act_item,null));
+       }else if(viewType == SECKILL){
+           return new SeckillViewHolder(mContext,mLayoutInflater.inflate(R.layout.seckill_item,null));
+       }else if(viewType == RECOMMEND){
+           return new RecommendViewHolder(mContext,mLayoutInflater.inflate(R.layout.recommend_item,null));
        }
         return null;
     }
 
+
+    class RecommendViewHolder extends RecyclerView.ViewHolder{
+        private Context mContext;
+        public RecommendViewHolder(final Context mContext,View itemView){
+            this.mContext = mContext;
+        }
+    }
+    class SeckillViewHolder extends RecyclerView.ViewHolder{
+        private final Context mContext;
+        private TextView tv_time_seckill;
+        private TextView tv_more_seckill;
+        private RecyclerView rv_seckill;
+        private SeckillRecyclerViewAdapter adapter;
+
+        //秒杀倒计时的相差秒数
+        private long dt = 0;
+
+        private Handler handler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                dt = dt - 1000;
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+                String time = sdf.format(new Date(dt));
+                tv_time_seckill.setText(time);
+
+                handler.removeMessages(0);
+                handler.sendEmptyMessageDelayed(0,1000);
+                if(dt<=0){
+                    //把消息移除
+                    handler.removeCallbacksAndMessages(null);
+                }
+            }
+        };
+
+
+        public SeckillViewHolder(Context mContext,View itemView) {
+            super(itemView);
+            this.mContext = mContext;
+            tv_time_seckill = (TextView) itemView.findViewById(R.id.tv_time_seckill);
+            tv_more_seckill = (TextView) itemView.findViewById(R.id.tv_more_seckill);
+            rv_seckill = (RecyclerView) itemView.findViewById(R.id.rv_seckill);
+        }
+
+        public void setData(ResultBeanData.ResultBean.SeckillInfoBean seckill_info) {
+            //1.得到数据了
+            //2.设置数据: 文本和RecyclerView的数据
+            adapter = new SeckillRecyclerViewAdapter(mContext,seckill_info.getList());
+            rv_seckill.setAdapter(adapter);
+
+            //设置布局管理器
+            rv_seckill.setLayoutManager(new LinearLayoutManager(mContext,LinearLayoutManager.HORIZONTAL,false));
+            //设置item的点击事件
+            adapter.setOnSeckillRecyclerView(new SeckillRecyclerViewAdapter.OnSeckillRecyclerView() {
+                @Override
+                public void onItemClick(int position) {
+                    Toast.makeText(mContext,"秒杀的位置:"+position,Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            //秒杀倒计时 - 毫秒
+
+            dt = Long.valueOf(seckill_info.getEnd_time()) - Long.valueOf(seckill_info.getStart_time());
+//            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+//            String time = sdf.format(new Date(dt));
+//            tv_time_seckill.setText(time);
+            handler.sendEmptyMessageDelayed(0,1000);
+        }
+    }
+    class ActionViewHolder extends RecyclerView.ViewHolder{
+        private Context mContext;
+        private ViewPager act_viewpager;
+        public ActionViewHolder(final Context mContext,View itemView){
+            super(itemView);
+            this.mContext = mContext;
+            act_viewpager = (ViewPager)itemView.findViewById(R.id.act_viewpager);
+        }
+
+        public void setData(final List<ResultBeanData.ResultBean.ActInfoBean> act_info) {
+            act_viewpager.setPageMargin(20);
+            act_viewpager.setOffscreenPageLimit(3);//>=3
+
+
+//setPageTransformer 决定动画效果
+            act_viewpager.setPageTransformer(true, new
+                //    RotateDownPageTransformer());
+                    AlphaPageTransformer(new ScaleInTransformer()) );
+            //1.得到shuju
+            //2.设置适配器
+            act_viewpager.setAdapter(new PagerAdapter() {
+                @Override
+                public int getCount() {
+                    return act_info.size();
+                }
+
+                //view 页面 与 object instaniateItem方法返回的值是否是同一个对象
+                @Override
+                public boolean isViewFromObject(View view, Object object) {
+                    return view == object;
+                }
+
+                @Override
+                public Object instantiateItem(ViewGroup container, int position) {
+                    ImageView imageView = new ImageView(mContext);
+                    imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+
+                    Glide.with(mContext).load(Constants.BASE_URL_IMAGE+act_info.get(position).getIcon_url()).into(imageView);
+                    //添加到容器中 Container
+                    container.addView(imageView);
+
+                    //设置点击事件
+
+                    return imageView;
+                }
+
+                @Override
+                public void destroyItem(ViewGroup container, int position, Object object) {
+                    container.removeView((View)object);
+                }
+            });
+        }
+    }
+    class ChannelViewHolder extends RecyclerView.ViewHolder{
+        private Context mContext;
+        private GridView gv_channel;
+        private ChannelAdapter adapter;
+
+        public ChannelViewHolder(final Context mContext, View itemView){
+            super(itemView);
+            this.mContext = mContext;
+            gv_channel = (GridView) itemView.findViewById(R.id.gv_channel);
+
+            //设置item的点击事件
+            gv_channel.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Toast.makeText(mContext,"position"+position,Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        public void setData(List<ResultBeanData.ResultBean.ChannelInfoBean> channel_info) {
+            //得到数据
+            //设置GrideView的适配器
+            adapter = new ChannelAdapter(mContext,channel_info);
+            gv_channel.setAdapter(adapter);
+        }
+    }
     class BannerViewHolder extends RecyclerView.ViewHolder{
 
         private Context mContext;
@@ -134,7 +303,7 @@ public class HomeFragmentAdapter extends RecyclerView.Adapter {
                 currentType = HOT;
                 break;
         }
-        return super.getItemViewType(position);
+        return currentType;
     }
 
     /**
@@ -145,6 +314,15 @@ public class HomeFragmentAdapter extends RecyclerView.Adapter {
             if(getItemViewType(position) == BANNER){
                 BannerViewHolder bannerViewHolder = (BannerViewHolder) holder;
                 bannerViewHolder.setData(resultBean.getBanner_info());
+            }else if(getItemViewType(position) == CHANNEL){
+                ChannelViewHolder channelViewHolder = (ChannelViewHolder) holder;
+                channelViewHolder.setData(resultBean.getChannel_info());
+            }else if(getItemViewType(position) == ACTION){
+                ActionViewHolder actionViewHolder = (ActionViewHolder) holder;
+                actionViewHolder.setData(resultBean.getAct_info());
+            }else if(getItemViewType(position) == SECKILL){
+                SeckillViewHolder seckillViewHolder = (SeckillViewHolder) holder;
+                seckillViewHolder.setData(resultBean.getSeckill_info());
             }
     }
 
@@ -155,6 +333,6 @@ public class HomeFragmentAdapter extends RecyclerView.Adapter {
     @Override
     public int getItemCount() {
         //开发过程中从1到6
-        return 1;
+        return 4;
     }
 }
